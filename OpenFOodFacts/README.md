96e00a554563305546666353c50c9832f31b6b2a
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2023-03-18 18:36:51 +0000

    punto 1

--- OpenFOodFacts/README.md
+++ OpenFOodFacts/README.md
@@ -1,6 +1,6 @@
-# OpenFoodfacts
+# OpenFOodFacts
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
 
 ## Development server
 
--- OpenFOodFacts/src/app/app.module.ts
+++ OpenFOodFacts/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { CaratteristicComponent } from './caratteristic/caratteristic.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent,
-    CaratteristicComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
