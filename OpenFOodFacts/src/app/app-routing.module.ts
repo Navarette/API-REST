b443a197cec05a8578cd71e232b21fedaa346e52
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2023-03-18 19:03:07 +0000

    punto 4

--- OpenFOodFacts/src/app/app-routing.module.ts
+++ OpenFOodFacts/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: 'search', component: SearchComponent },
-  { path: 'caratteristics/:id', component: CaratteristicsComponent }
+  // { path: 'caratteristics/:id', component: CaratteristicsComponent }
 
 ];
 
--- OpenFOodFacts/src/app/search/search.component.ts
+++ OpenFOodFacts/src/app/search/search.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+  constructor(private api: ApiService) { }
 
+  ngOnInit(): void {
+
+  }
+
+  search(e: any) {
+    var q = e.target.value;
+    this.api.products(q).subscribe((data: any) => this.data = data);
+    console.log(this.data)
+  }
+  data(data: any) {
+    throw new Error('Method not implemented.');
+  }
 }
