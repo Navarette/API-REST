e137d4ab651bcdd0a83b8e86001ff6a0e824e7b1
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2023-03-18 18:39:33 +0000

    punto 2

--- OpenFOodFacts/src/app/app-routing.module.ts
+++ OpenFOodFacts/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'caratteristics/:id', component: CaratteristicsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
